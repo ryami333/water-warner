cf60b93e349237ee8e6b56d3b3258f4eff2ad9bb
Mitch Ryan <mitch@diesdas.digital>
2025-10-11 18:59:53 +0200

    Move electron-forge config to discrete file

---
+++ forge.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * @type {import("electron-forge/shared-types").ForgeConfig} config
+ */
+const config = {
+  packagerConfig: {
+    name: "Water Warner",
+    executableName: "Water Warner",
+    icon: "./icons/icon",
+  },
+  makers: [
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
+  ],
+};
+
+export default config;
