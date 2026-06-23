53cb0eb6b28fe26a157bad49add2ddc77b29400e
Mitch Ryan <mitch@diesdas.digital>
2026-06-23 10:55:08 +0200

    Consolidate typescript-eslint dependencies, remove unneeded eslint config workaround

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,20 @@
 // @ts-check
 import eslintJs from "@eslint/js";
-import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
-import typescriptEslintParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
   globalIgnores(["dist/", "out/"]),
+  eslintJs.configs.recommended,
+  tseslint.configs.recommended,
   {
     files: ["**/*.{js,jsx,ts,tsx,mjs}"],
     languageOptions: {
-      parser: typescriptEslintParser,
       globals: {
         ...globals.node,
         ...globals.browser,
       },
-    },
-
-    plugins: {
-      // @ts-ignore: known upstream issue.
-      "@typescript-eslint": typescriptEslintPlugin,
-    },
-
-    rules: {
-      ...eslintJs.configs.recommended.rules,
-      ...typescriptEslintPlugin.configs.recommended.rules,
     },
   },
 ]);
